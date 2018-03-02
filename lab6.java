12a4cfa6a0d930cf284ab4b1289687a79175d0b5
kevensinmelendez359 <kevensinmelendez359@gmail.com>
2018-03-01 19:19:59 -0500

    uploaded lab 6

---
+++ lab6.java
@@ -0,0 +1,24 @@
+/*
+  Keven Melendez
+  CoSci290
+*/
+
+public class lab6
+{
+
+public static void main(String[] args)
+{
+  //Array initializer
+  int[] numbers = {15 , 25, 35, 45};
+  
+  //for allows each array to be printed out
+  for(int i = 0; i < numbers.length; i++)
+  {
+    System.out.println(numbers[i] + " is array " + i + "\n");
+  }
+} 
+
+//no code after this
+}
+
+
